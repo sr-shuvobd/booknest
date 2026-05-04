061d36f8988a31c6f4e01f4e99b331cf9b3de7ba
sr-shuvobd <mdshohanurrahomanshuvo@gmail.com>
2026-05-04 21:41:15 +0600

    update page upgrade

--- src/app/update/page.jsx
+++ src/app/update/page.jsx
@@ -31,7 +31,7 @@ const UpdatePage = () => {
   };
 
   return (
-    <div className="md:min-h-screen flex justify-center items-center">
+    <div className="md:min-h-screen flex justify-center items-center bg-amber-50">
       <div className="shadow p-10 rounded-2xl">
         <h1 className="font-bold text-2xl mb-3">
           Update Your Profile Information
